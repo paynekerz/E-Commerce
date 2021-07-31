c86d2bb74a2eed5aafde8df3e53f8c9ef745b3ce
Payne Kerz <paynekerz@gmail.com>
2021-07-30 19:40:28 -0700

    index.js done

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,32 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+
+    },
+    
+    productName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    productPrice: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    category: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
   },
   {
     sequelize,
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,9 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    product_id: {
+      
+    }
   },
   {
     sequelize,
